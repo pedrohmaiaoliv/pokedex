1155c1957402cb3f2a8b8efbf28a8f77e4f002cf
pedrohmaiaoliv <pedrohmaiaoliv@gmail.com>
2023-12-16 11:47:13 -0300

    Projetinho Pokedex com React

---
+++ src/components/Skeletons/index.jsx
@@ -0,0 +1,9 @@
+import { Skeleton } from '@mui/material'
+import React from 'react'
+
+export const Skeletons = () => {
+  return <Skeleton variant="rounded" width={210} height={60} />
+  return <Skeleton variant="rounded" width={210} height={60} />
+  return <Skeleton variant="rounded" width={210} height={60} />
+  
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import PokemonCard from '../components/PokemonCard'
+import { Container, Grid, Skeleton } from '@mui/material'
+import axios from 'axios'
+
+export const Home = () => {
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        getPokemons()
+    }, []);
+
+    const getPokemons = () => {
+        var endpoints = [];
+        for(var i = 1; i <= 1017; i++){
+            endpoints.push(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+        }
+        
+        axios.all(endpoints.map((endpoint) => axios.get(endpoint))).then((res) => setPokemons(res));
+
+    };
+    const pokemonFilter = (name) => {
+        var filteredPokemons = []
+        if(name==="") {
+            getPokemons();
+        }
+        for (var i in pokemons) {
+            if(pokemons[i].data.name.includes(name)) {
+                filteredPokemons.push(pokemons[i]);
+            }
+        }
+        setPokemons(filteredPokemons);
+    };
+
+
+  return (
+    <div>
+        <Navbar pokemonFilter={pokemonFilter}/>
+        <Container maxWidth="false">
+            <Grid container spacing={3}>
+                {pokemons.length === 0 ?  <Skeleton/>  :
+                pokemons.map((pokemon, key) => (
+                    <Grid item xs={12} sm={6} m={4} lg={2} key={key}>
+                        <PokemonCard name={pokemon.data.name} image={pokemon.data.sprites.front_default} types={pokemon.data.types}/>
+                    </Grid>
+                ))
+                }
+                
+            </Grid>
+        </Container>
+    </div>
+  )
+}
